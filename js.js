af87ab90189dd4fd1532154018c7271d386ffbd8
OkamiAka <nils.caqueux@gmail.com>
2023-03-14 19:54:18 +0100

    premier test etat: valider ?redefinir test2

--- js.js
+++ js.js
@@ -1,7 +1,30 @@
 const lists = document.querySelector(".alllist");
-function list(){
+let idlist = 0;
+function creatlist() {
+    idlist++;
+
     const list = document.createElement("div");
+    list.setAttribute("id",`list-${idlist}`)
     list.classList.add("list");
     lists.appendChild(list);
+
+    const titlelist = document.createElement("h2");
+    titlelist.classList.add("titlelist");
+    titlelist.textContent = prompt("Nom de la liste:");
+    list.appendChild(titlelist);
+
+    const buttonaddlist = document.createElement("button");
+    buttonaddlist.classList.add("add");
+    buttonaddlist.textContent="+"
+    buttonaddlist.setAttribute("onclick",`a_faire(${idlist})`)
+    list.appendChild(buttonaddlist);
 }
-list();
+
+//     const textlist = document.createElement("details");
+//     textlist.classList.add("textlist");
+//     list.appendChild(textlist);
+
+//     const nomlist = document.createElement("summary");
+//     nomlist.classList.add("nomlist");
+//     nomlist.textContent = "test";
+//     textlist.appendChild(nomlist);
